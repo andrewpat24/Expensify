326b86b4a5151c1691c2cceec8b6a17c424a7b07
Andrew Patterson <apat24@mail.sfsu.edu>
2019-05-17 18:50:00 -0700

    Refactors EditExpense to be class based component

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -4,34 +4,39 @@ import ExpenseForm from "./ExpenseForm";
 // Helpers
 import { editExpense, removeExpense } from "../actions/expenses";
 
-const EditExpense = props => {
-  if (typeof props.expense === "undefined") {
-    return props.history.push("/");
+export class EditExpense extends React.Component {
+  constructor(props) {
+    super(props);
+    if (typeof props.expense === "undefined") {
+      return props.history.push("/");
+    }
   }
 
-  return (
-    <div>
-      <h1>Editing doc id: {props.match.params.id}</h1>
-      <h2>{props.expense.description}</h2>
-      <ExpenseForm
-        expense={props.expense}
-        expenseIsNew={false}
-        onSubmit={expense => {
-          props.dispatch(editExpense(props.expense.id, expense));
-          props.history.push("/");
-        }}
-      />
-      <button
-        onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id }));
-          props.history.push("/");
-        }}
-      >
-        Remove
-      </button>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <h1>Editing doc id: {this.props.match.params.id}</h1>
+        <h2>{this.props.expense.description}</h2>
+        <ExpenseForm
+          expense={this.props.expense}
+          expenseIsNew={false}
+          onSubmit={expense => {
+            this.props.dispatch(editExpense(this.props.expense.id, expense));
+            this.props.history.push("/");
+          }}
+        />
+        <button
+          onClick={() => {
+            this.props.dispatch(removeExpense({ id: this.props.expense.id }));
+            this.props.history.push("/");
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, props) => {
   return {
@@ -42,3 +47,32 @@ const mapStateToProps = (state, props) => {
 };
 
 export default connect(mapStateToProps)(EditExpense);
+
+// const EditExpense = props => {
+//   if (typeof props.expense === "undefined") {
+//     return props.history.push("/");
+//   }
+
+//   return (
+//     <div>
+//       <h1>Editing doc id: {props.match.params.id}</h1>
+//       <h2>{props.expense.description}</h2>
+//       <ExpenseForm
+//         expense={props.expense}
+//         expenseIsNew={false}
+//         onSubmit={expense => {
+//           props.dispatch(editExpense(props.expense.id, expense));
+//           props.history.push("/");
+//         }}
+//       />
+//       <button
+//         onClick={() => {
+//           props.dispatch(removeExpense({ id: props.expense.id }));
+//           props.history.push("/");
+//         }}
+//       >
+//         Remove
+//       </button>
+//     </div>
+//   );
+// };
