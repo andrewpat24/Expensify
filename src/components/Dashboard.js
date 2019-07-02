6bd0ec6799ec1310a55231799c6d6f29e67cc6bd
Andrew Patterson <apat24@mail.sfsu.edu>
2019-07-01 17:45:04 -0700

    Adds new Expenses Summary component with snapshot test. Adds Expenses Summary to dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,10 +2,12 @@ import React from "react";
 // Components
 import ExpenseList from "./ExpenseList";
 import ExpenseListFilters from "./ExpenseListFilters";
+import ExpensesSummary from "./ExpensesSummary";
 
 const Dashboard = () => (
   <section component="Dashboard">
     <h1>Dashboard</h1>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </section>
