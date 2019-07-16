269c9b1e39a9271fb9af56f59413f48a7e865415
Andrew Patterson <apat24@mail.sfsu.edu>
2019-07-16 12:29:19 -0700

    Adds polyfill to webpack. Adds test for loading component.

--- src/tests/components/Loader.test.js
+++ src/tests/components/Loader.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import { Loading } from "../../components/Loading";
 
-test("Should render loader", () => {
+test("Should render Loading component", () => {
   const wrapper = shallow(<Loading />);
   expect(wrapper).toMatchSnapshot();
 });
